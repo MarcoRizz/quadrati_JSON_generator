155ad4d6068543bd05851d5eac09a2226ff41ece
MarcoRizz <65336645+MarcoRizz@users.noreply.github.com>
2024-09-18 12:32:20 +0200

    matrice lettere casuale

--- genetate_JSON.cpp
+++ genetate_JSON.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>  // Per rand() e srand()
+#include <ctime>    // Per time()
 #include "libs/json.hpp"
 
 using json = nlohmann::json;
 
 int main() {
+    // Inizializza il seme del generatore di numeri casuali
+    std::srand(std::time(0));
+
     // Creiamo il contenuto JSON
     json data;
 
-    // Dati per "today" e "yesterday"
-    json grid = {
-        {"A", "B", "C", "D"},
-        {"E", "F", "G", "H"},
-        {"I", "J", "K", "L"},
-        {"M", "N", "O", "P"}
-    };
+    // Generazione della griglia di lettere casuali 4x4
+    json grid = json::array();
+    for (int i = 0; i < 4; ++i) {
+        json row = json::array();
+        for (int j = 0; j < 4; ++j) {
+            // Genera una lettera casuale tra 'A' e 'Z'
+            char random_letter = 'A' + std::rand() % 26;
+            row.push_back(std::string(1, random_letter));
+        }
+        grid.push_back(row);
+    }
 
+    //qui devo calcolare l'elenco di parole trovate all'interno della griglia (TODO)
+    json words = {"stocazzo", "pippo", "giovanni", "francesco", "gianfrancesco"};
+
+    //qui devo calcolare tutte le possibilità e calcolare quali parole possono passare da ciascuna lettera (TODO)
     json grid_links = {
         {{0, 4}, {0, 1, 2, 3, 4}, {0}, {3}},
         {{0}, {0, 1, 2}, {0}, {2, 3}},
