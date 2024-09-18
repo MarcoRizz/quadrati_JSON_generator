720ad65c75ec5ae10cb212d0adb07539ca7ab80f
MarcoRizz <65336645+MarcoRizz@users.noreply.github.com>
2024-09-18 15:49:15 +0200

    words incrementale

--- genetate_JSON.cpp
+++ genetate_JSON.cpp
@@ -9,10 +9,10 @@
 const int DIM1 = 4;
 const int DIM2 = 4;
 const int MAX_LENGTH = 16; // Lunghezza massima delle parole
-int MAX_WORDS = 10;  // Numero massimo di parole  //sarà da sostituire col numero di parole trovate (TODO)
 
 String generate_random_word(int max_length);
-void add_element(int*& array, int& size, int value);
+void add_word(std::string*& array, int& size, const std::string& word);
+void add_element_to_ij(int*& array, int& size, int value);
 
 int main() {
     // Inizializza il seme del generatore di numeri casuali
@@ -35,39 +35,42 @@ int main() {
     /***********************************************************************************
     // CREAZIONE ARRAY-1D WORDS
     ***********************************************************************************/
-    String words[MAX_WORDS];
-
-    // Popoliamo l'array con parole casuali (TEMP)
-    for (int i = 0; i < MAX_WORDS; ++i) {
-        words[i] = generate_random_word(MAX_LENGTH);
-    }
+    int words_size = 0;
+    String* words = nullptr;
 
     //qui devo calcolare tutte le possibilità e calcolare quali parole possono passare da ciascuna lettera (TODO)
+
+    // Popoliamo l'array con parole casuali (TEMP)
+    int num_new_words = 3 + std::rand() % 5; // Numero casuale di parole tra 3 e 7
+    for (int i = 0; i < num_new_words; ++i) {
+        add_word(words, words_size, generate_random_word(MAX_LENGTH));
+    }
 
     /***********************************************************************************
     // CREAZIONE ARRAY-3D GRID_LINKS
     ***********************************************************************************/
-    // la terza dimensione è variabile e archiviata in link_sizes
+    // la terza dimensione è variabile e archiviata in links_size
     // per mantenere la terza dimensione variabile lavoro coi puntatori
     int*** grid_links = new int**[DIM1];
-    int** link_sizes = new int*[DIM1]; // Array per memorizzare le dimensioni della terza dimensione
+    int** links_size = new int*[DIM1]; // Array per memorizzare le dimensioni della terza dimensione
     for (int i = 0; i < DIM1; ++i) {
         grid_links[i] = new int*[DIM2];
-        link_sizes[i] = new int[DIM2];
+        links_size[i] = new int[DIM2];
         for (int j = 0; j < DIM2; ++j) {
             // Imposta dimensione variabile per la terza dimensione
-            link_sizes[i][j] = 0; // Memorizza la dimensione della terza dimensione per ogni [i][j]
+            links_size[i][j] = 0; // Memorizza la dimensione della terza dimensione per ogni [i][j]
             grid_links[i][j] = nullptr; // Puntatore inizialmente nullo
         }
     }
 
-    //assegno i link tra lettere e parole
+    //qui devo assegnare i link tra lettere e parole
+
     // Popola con valori casuali (TEMP)
-    int num_new_elements = 1 + std::rand() % (MAX_WORDS * DIM1 * DIM2); // Numero casuale di nuovi elementi
+    int num_new_elements = 1 + std::rand() % (words_size * DIM1 * DIM2); // Numero casuale di nuovi elementi
     for (int k = 0; k < num_new_elements; ++k) {
         int casual_i = std::rand() % DIM1;
         int casual_j = std::rand() % DIM2;
-        add_element(grid_links[casual_i][casual_j], link_sizes[casual_i][casual_j], std::rand() % 10); // Aggiungo elemento a grid_links[casual_i][casual_j]
+        add_element_to_ij(grid_links[casual_i][casual_j], links_size[casual_i][casual_j], std::rand() % 10); // Aggiungo elemento a grid_links[casual_i][casual_j]
     }
 
     //se una lettera rimane priva di link, dovrò sostituirla e ripetere il calcolo (TODO)
@@ -87,8 +90,8 @@ int main() {
 
     // converto le parole in JSON
     json words_json = json::array();
-    for (const auto& word : words) {
-        words_json.push_back(word);
+    for (int i = 0; i < words_size; ++i) {
+        words_json.push_back(words[i]);
     }
 
     // Converti grid_links in JSON
@@ -97,7 +100,7 @@ int main() {
         json json_row = json::array();
         for (int j = 0; j < DIM2; ++j) {
             json json_link = json::array();
-            int num_links = link_sizes[i][j]; // Ottieni la dimensione della terza dimensione per [i][j]
+            int num_links = links_size[i][j]; // Ottieni la dimensione della terza dimensione per [i][j]
             for (int k = 0; k < num_links; ++k) {
                 json_link.push_back(grid_links[i][j][k]);
             }
@@ -124,15 +127,16 @@ int main() {
     }
 
     // Deallocazione della memoria
+    delete[] words; // Dealloca la memoria delle parole
     for (int i = 0; i < DIM1; ++i) {
         for (int j = 0; j < DIM2; ++j) {
             delete[] grid_links[i][j]; // Dealloca la memoria della terza dimensione
         }
         delete[] grid_links[i]; // Dealloca la memoria della seconda dimensione
-        delete[] link_sizes[i]; // Dealloca la memoria delle dimensioni della terza dimensione
+        delete[] links_size[i]; // Dealloca la memoria delle dimensioni della terza dimensione
     }
     delete[] grid_links; // Dealloca la memoria della prima dimensione
-    delete[] link_sizes; // Dealloca la memoria delle dimensioni della terza dimensione
+    delete[] links_size; // Dealloca la memoria delle dimensioni della terza dimensione
 
     return 0;
 }
@@ -147,8 +151,19 @@ String generate_random_word(int max_length) {
     return word;
 }
 
+// Funzione per aggiungere una nuova stringa a un array dinamico di stringhe
+void add_word(std::string*& array, int& size, const std::string& word) {
+    int new_size = size + 1;
+    std::string* new_array = new std::string[new_size];
+    std::memcpy(new_array, array, size * sizeof(std::string));
+    delete[] array;
+    array = new_array;
+    array[size] = word;
+    size = new_size;
+}
+
 // Funzione per aggiungere un nuovo elemento a un array dinamico
-void add_element(int*& array, int& size, int value) {
+void add_element_to_ij(int*& array, int& size, int value) {
     // Realloca l'array se necessario
     int new_size = size + 1;
     int* new_array = new int[new_size];
