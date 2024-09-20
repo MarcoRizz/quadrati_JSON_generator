80fef25618b034282fd44e27841804032bf1c0e7
MarcoRizz <65336645+MarcoRizz@users.noreply.github.com>
2024-09-20 20:06:31 +0200

    rimosso limite massimo di parole
    era utilizzato in fase di test del codice

--- tools/dict_pointer_generator.cpp
+++ tools/dict_pointer_generator.cpp
@@ -146,13 +146,12 @@ private:
 };
 
 int main() {
-    auto timer_overall_start = std::chrono::high_resolution_clock::now();
+    auto timer_start = std::chrono::high_resolution_clock::now();
     Dizionario dizionario;
 
     //---------------------------------------------
     //sezione di creazione del dizionario
 
-    int max_words = 1000000;
     std::ifstream file(DICTIONARY_PATH);
     String line;
     // Verifica se il file è stato aperto correttamente
@@ -161,19 +160,14 @@ int main() {
         return 0;
     }
 
-    int word_i = 0;
-    while (word_i < max_words) {
-        if (!std::getline(file, line)) {
-            break; // Esci se non ci sono più righe
-        }
+    while (std::getline(file, line)) {
         dizionario.inserisciParola(line);
-        word_i++;
     }
     
     auto timer_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> duration = timer_end - timer_overall_start;
+    std::chrono::duration<double, std::milli> duration = timer_end - timer_start;
     std::cout << "Elapsed time: " << duration.count() << " ms" << std::endl;
-
+    
     /*// Inserimento di parole nel dizionario
     dizionario.inserisciParola("cane");
     dizionario.inserisciParola("casa");
@@ -196,17 +190,28 @@ int main() {
     int totaleParole = dizionario.contaParole();
     std::cout << "\nNumero totale di parole nel dizionario: " << totaleParole << std::endl;
 
+    timer_start = std::chrono::high_resolution_clock::now();
+
     // Serializzazione del dizionario
     std::string percorsoFile = "dizionario.json";
     if (dizionario.salvaInFileCompatto(percorsoFile)) {
         std::cout << "Dizionario salvato correttamente in \"" << percorsoFile << "\"." << std::endl;
     }
 
+    timer_end = std::chrono::high_resolution_clock::now();
+    duration = timer_end - timer_start;
+    std::cout << "Operation time: " << duration.count() << " ms" << std::endl;
+    timer_start = std::chrono::high_resolution_clock::now();
+
     // Creazione di un nuovo dizionario e deserializzazione da file
     Dizionario nuovoDizionario;
     if (nuovoDizionario.caricaDaFileCompatto(percorsoFile)) {
         std::cout << "Dizionario caricato correttamente da \"" << percorsoFile << "\"." << std::endl;
     }
+
+    timer_end = std::chrono::high_resolution_clock::now();
+    duration = timer_end - timer_start;
+    std::cout << "Operation time: " << duration.count() << " ms" << std::endl;
 
     // Verifica delle parole nel nuovo dizionario
     for (const auto& parola : paroleDaCercare) {
