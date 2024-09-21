4c07b92689a390cae1518d68b3d0eb4cc2cb6fa3
MarcoRizz <65336645+MarcoRizz@users.noreply.github.com>
2024-09-21 03:13:38 +0200

    duplicato completePath

--- findPath.cpp
+++ findPath.cpp
@@ -14,7 +14,7 @@ bool isValid(int x, int y) {
     return x >= 0 && x < DIM1 && y >= 0 && y < DIM2 && !visited[x][y];
 }
 
-void completePath(int pathLength) {
+void returnFinalWord(int pathLength) {
     String parola;
     for (int i = 0; i < pathLength; ++i) {
         parola += grid[path[i].first][path[i].second];
@@ -24,19 +24,46 @@ void completePath(int pathLength) {
     }
 }
 
+//cerca tutti i possibili percorsi nella griglia
 void findPaths(int x, int y, int step, int path_size) {
     path[step] = {x, y};
     visited[x][y] = true;
 
     // Se abbiamo raggiunto il numero di passi massimo, stampiamo il percorso
     if (step + 1 == path_size) {
-        completePath(step + 1);
+        returnFinalWord(step + 1);
     } else {
         for (int i = 0; i < 8; ++i) {
             int newX = x + directions[i].first;
             int newY = y + directions[i].second;
             if (isValid(newX, newY)) {
                 findPaths(newX, newY, step + 1, path_size);
+            }
+        }
+    }
+
+    // Backtracking
+    visited[x][y] = false;
+}
+
+void returnFinalPath(int pathLength) {
+    //TODO
+}
+
+//cerca tutti i possibili percorsi di una specifica parola
+void findWordPaths(int x, int y, int step, String word) {
+    path[step] = {x, y};
+    visited[x][y] = true;
+
+    // Se abbiamo raggiunto il numero di passi massimo, stampiamo il percorso
+    if (step + 1 == word.size()) {
+        returnFinalPath(step + 1);
+    } else {
+        for (int i = 0; i < 8; ++i) {
+            int newX = x + directions[i].first;
+            int newY = y + directions[i].second;
+            if (isValid(newX, newY) && grid[path[i].first][path[i].second] == word[step]) {
+                findWordPaths(newX, newY, step + 1, word);
             }
         }
     }
