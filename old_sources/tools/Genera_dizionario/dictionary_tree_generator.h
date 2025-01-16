a90680b01785764ab4940ee5abe7e94da4e820df
MarcoRizz <65336645+MarcoRizz@users.noreply.github.com>
2025-01-16 22:25:53 +0100

    removing old files

--- old_sources/tools/Genera_dizionario/dictionary_tree_generator.h
+++
@@ -1,9 +0,0 @@
-#ifndef DICTIONARY_TREE_GENERATOR_H
-#define DICTIONARY_TREE_GENERATOR_H
-
-#include "../../Classes/Dizionario.h"
-#include "../../libs/json.hpp"
-
-#define PERCORSO_TREE_DICTIONARY "..\\Dizionari\\dizionario.json"
-
-#endif
